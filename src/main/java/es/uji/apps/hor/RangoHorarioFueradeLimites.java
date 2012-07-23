19aa2e5f469e8205b90d56f35581965453274f9f
David Rubert <david.rubert@si.uji.es>
2012-07-23 09:58:08 +0000

    HOR-65: añadir control para que al guardar una hora inicio y fin, no existan clases que no esten en el intervalo de horas https://universitatjaumei.jira.com/browse/HOR-65
    Solucionado
    
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@9634 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/es/uji/apps/hor/RangoHorarioFueradeLimites.java
+++ src/main/java/es/uji/apps/hor/RangoHorarioFueradeLimites.java
@@ -1,16 +1,10 @@
 package es.uji.apps.hor;
 
-
 @SuppressWarnings("serial")
 public class RangoHorarioFueradeLimites extends GeneralHORException
 {
-    public RangoHorarioFueradeLimites(String msg)
-    {
-        super("Les hores sel·leccionades no son vàlides, ja que existeixen registres en el calendari que s'ixen de la franja horària");
-    }
-
     public RangoHorarioFueradeLimites()
     {
-        super();
+        super("Les hores sel·leccionades no son vàlides, ja que existeixen registres en el calendari que s'en ixen de la franja horària");
     }
 }
