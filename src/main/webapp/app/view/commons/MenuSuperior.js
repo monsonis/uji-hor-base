88ed7fd5d1138d843f76510a2591ab937a974803
David Rubert <david.rubert@si.uji.es>
2012-11-16 07:54:34 +0000

    HOR-129: El menú desplegable que muestra las diferentes pantallas de la aplicación debería desplegarse al pinchar sobre el primer elemento. Es fundamental para iniciar el testing en cliente. http://jira.uji.es/browse/HOR-129
    Solucionado.
    
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@11698 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/webapp/app/view/commons/MenuSuperior.js
+++ src/main/webapp/app/view/commons/MenuSuperior.js
@@ -3,6 +3,7 @@ Ext.define('HOR.view.commons.MenuSuperior',
     extend : 'Ext.toolbar.Toolbar',
     alias : 'widget.menuSuperior',
     width : 200,
+    menuVisible: false,
     items : [
     {
         xtype : 'splitbutton',
@@ -11,20 +12,29 @@ Ext.define('HOR.view.commons.MenuSuperior',
         menu :
         {
             xtype : 'menu',
+            titleCollapse: true,
             items : [
             {
                 text : 'Gestió d\'horaris',
-                action: 'gestion-horarios'
+                action : 'gestion-horarios'
             },
             {
                 text : 'Dates del curs acadèmic',
-                action: 'curso-academico'
+                action : 'curso-academico'
             },
             {
                 text : 'Assignació d\'aules',
-                action: 'asignacion-aulas'
-            }]
+                action : 'asignacion-aulas'
+            } ]
+        },
+        handler: function(button, event) {
+            if (!this.menuVisible) {
+                button.showMenu();
+                this.menuVisible = true;
+            } else {
+                button.hideMenu();
+                this.menuVisible = false;
+            }
         }
-
     } ]
 });
