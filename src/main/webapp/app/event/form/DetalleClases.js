c928fdcb55804a82b3c6171f7aee83592c022a0f
Maria Sirvent <msirvent@uji.es>
2012-11-16 09:07:51 +0000

    IN PROGRESS - issue HOR-124: Cambiar la representación de la visualización de fechas en las que se impartirá docencia para que se visualicen las fechas de exámenes de manera diferente http://jira.uji.es/browse/HOR-124 Solucionado
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@11706 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/webapp/app/event/form/DetalleClases.js
+++ src/main/webapp/app/event/form/DetalleClases.js
@@ -3,38 +3,45 @@ Ext.define('Event.form.DetalleClases',
     extend : 'Ext.form.FieldSet',
     alias : 'widget.detalleClases',
     title : 'La classe tindrà docència els dies:',
-    layout: {
-        type: 'table',
-        columns: 6
+    layout :
+    {
+        type : 'table',
+        columns : 6
     },
     hidden : true,
-    
+
     style :
     {
-        marginTop: '20px'
+        marginTop : '20px'
     },
-    
-    items : [ ],
-    
+
+    items : [],
+
     actualizarDetalleClases : function(clases)
     {
         this.removeAll();
-        
-        for (var i=0; i < clases.length; i++)
+
+        for ( var i = 0; i < clases.length; i++)
         {
             if (clases[i].docencia == 'S')
             {
                 var fecha = Ext.Date.parse(clases[i].fecha, "d\/m/\Y H:i:s");
-                
+                fecha = Ext.Date.format(fecha, "d/m/Y");
+
+                if (clases[i].tipoDia == 'E')
+                {
+                    fecha = '*' + fecha;
+                }
+
                 var display = new Ext.create('Ext.form.field.Display',
                 {
-                    value : Ext.Date.format(fecha, "d/m/Y"),
+                    value : fecha,
                     style :
                     {
                         marginRight : '50px'
                     }
                 });
-            
+
                 this.add(display);
             }
         }
