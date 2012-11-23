77d82a03837b9b7c7e84f58a2904805343d1d228
Javier Muñoz <ferrar@uji.es>
2012-11-23 08:52:45 +0000

    Añadido test de integración para probar la asignación en la pantalla de asignación de aulas
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@11815 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/webapp/test-harness.js
+++ src/main/webapp/test-harness.js
@@ -6,7 +6,8 @@ Harness.configure({
 		'HOR' : 'app'
 	},
 
-	preload : [ '/hor/sinon/sinon-1.5.0.js', '/hor/sinon/sinon-init.js',
+	preload : [ '/hor/sinon/sinon-1.5.0.js', 	            
+	            '/hor/sinon/sinon-init.js',
 	// 'http://cdn.sencha.io/ext-4.1.0-gpl/resources/css/ext-all.css',
 	'/hor/Ext/ext-all-debug.css',
 			'/hor/extensible-1.5.1/resources/css/extensible-all.css',
@@ -45,7 +46,11 @@ Harness.start({
 	},
 	 {
 		hostPageUrl : 'index.html',
-		url : 'tests/900-Aplicacion/900-020_assignacioaules.t.js'
+		url : 'tests/900-Aplicacion/900-020_assignacioaules_combos.t.js'
+	},
+	{
+		hostPageUrl : 'index.html',
+		url : 'tests/900-Aplicacion/900-030_assignacioaules_tree.t.js'
 	}
 	]
 }
---
+++ src/main/webapp/tests/900-Aplicacion/900-030_assignacioaules_tree.t.js
@@ -0,0 +1,77 @@
+StartTest(function(t) {
+	t.waitForCQ('splitbutton', function(splitbutton) {
+		
+		var centro_seleccionado = 2;
+		var estudio_seleccionado = 1;
+		var tree_aulas;
+   	 	var grid_aulasasign;
+   	 	var selected_aula_id;
+		
+			
+		var steps = [
+		             { action : 'click', target: '>>splitbutton' }, 
+		             { action : 'click', target :'>>menuitem[action="asignacion-aulas]' },
+		             { waitFor : 'componentQuery', args : '>>combobox[alias=widget.comboCentros]'},
+		             function (next) {
+		            	tree_aulas = Ext.ComponentQuery.query('>>panel[alias=widget.treePanelAulas]')[0];		
+		            	 grid_aulasasign = Ext.ComponentQuery.query('>>panel[alias=widget.gridAulas]')[0];
+		            	
+		            	 t.ok(tree_aulas.disabled, 'El árbol de aulas disponibles está deshabilitado');
+		            	 t.ok(grid_aulasasign.disabled, 'El aula de aulas asignadas está deshabilitado');
+		            	 
+		            	 next();		            	
+		             },
+		             { action : 'click', target :'>>combobox[alias=widget.comboCentros]' }, 		             
+		             function (next) {
+		            	 t.ok(tree_aulas.disabled, 'El árbol de aulas disponibles sigue deshabilitado');
+		            	 t.ok(grid_aulasasign.disabled, 'El aula de aulas asignadas sigue deshabilitado');
+		            	 
+		            	 
+		            	 var combo_centros = Ext.ComponentQuery.query('>>combobox[alias=widget.comboCentros]')[0];			            	 
+		            	 t.click(combo_centros.getPicker().getNode(centro_seleccionado),next);		            	
+		             },
+		             { action : 'click', target :'>>combobox[alias=widget.comboEstudios]' }, 		             
+		             function (next) {		            	 
+		            	 var combo_estudio = Ext.ComponentQuery.query('>>combobox[alias=widget.comboEstudios]')[0];
+		            	 t.click(combo_estudio.getPicker().getNode(estudio_seleccionado), next);
+		             },
+		             function (next){
+		            	 t.ok(!tree_aulas.disabled, 'El árbol de aulas disponibles NO está deshabilitado');
+		            	 t.ok(tree_aulas.getRootNode().hasChildNodes(),'El árbol tiene contenido');
+		            			
+		            	 
+		            	 var node_edificio = tree_aulas.getRootNode().getChildAt(0);		            	 
+		            	 node_edificio.expand();
+		            	 t.ok(node_edificio.hasChildNodes(),'La carpeta edificio tiene contenido');
+		            	 
+		            	 var node_area = node_edificio.getChildAt(0);
+		            	 node_area.expand();
+		            	 t.ok(node_area.hasChildNodes(),'La carpeta area tiene contenido');
+		            	 
+		            	 var node_aula = node_area.getChildAt(1);
+		            	 console.log(node_aula.getId());
+		            	 tree_aulas.getSelectionModel().select(node_aula);
+		            	 
+		            	 next();
+
+		             },
+		             { action : 'click', target :'>>panel[alias=widget.treePanelAulas] button' }, 
+		             // Esperar a que se recarge
+		             function (next){	            	 
+		            	 var encontrado = grid_aulasasign.getStore().find('id',selected_aula_id);
+		            			            	 
+		            	 var has_been_inserted = encontrado>=0;
+		            	 
+		            	 t.ok(has_been_inserted,'El aula ha sido asignada');
+		            	 
+		            	 next();
+		             }
+
+		              			
+		         ];
+		
+		t.chain(steps);
+		
+	});
+
+});		             
