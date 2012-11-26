a0406956016e47e78c60ef518179731e6f375531
Maria Sirvent <msirvent@uji.es>
2012-11-26 13:34:55 +0000

    issue HOR-148: En el grid que muestra las aulas asignadas, añadir las siguientes columnas: Edificio/Tipo/Planta/Semestra, y permitir ordenar por columnas. http://jira.uji.es/browse/HOR-148
    Solucionado
    
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@11854 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/webapp/app/view/aulas/GridAulas.js
+++ src/main/webapp/app/view/aulas/GridAulas.js
@@ -1,21 +1,41 @@
 Ext.define('HOR.view.aulas.GridAulas',
 {
     extend : 'Ext.grid.Panel',
-    title: 'Aules asignades',
+    title : 'Aules asignades',
     alias : 'widget.gridAulas',
     store : 'StoreAulasAsignadas',
     disableSelection : false,
-    sortableColumns : false,
+    sortableColumns : true,
     tbar: [ {
         xtype: 'button',
         name: 'borrar',
         text: 'Esborrar aula',
         iconCls : 'application-add'
-    }],
+    } ],
     columns : [
     {
         text : 'Aula',
         dataIndex : 'nombre',
-        width: 250
-    }]
+        width : '40%',
+    },
+    {
+        text : 'Edificio',
+        dataIndex : 'edificio',
+        width : '15%'
+    },
+    {
+        text : 'Tipo',
+        dataIndex : 'tipo',
+        width : '15%'
+    },
+    {
+        text : 'Planta',
+        dataIndex : 'planta',
+        width : '15%'
+    },
+    {
+        text : 'Semestre',
+        dataIndex : 'semestreId',
+        width : '15%'
+    } ]
 });
