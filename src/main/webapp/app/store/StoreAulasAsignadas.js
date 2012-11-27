025bde2fcc0e9c91f87a42d38d383bfab07181d9
David Rubert <david.rubert@si.uji.es>
2012-11-27 07:25:27 +0000

    Mostramos el mensaje de error al cliente cuando hay un duplicado en la inserción de un aula asignada.
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@11856 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/webapp/app/store/StoreAulasAsignadas.js
+++ src/main/webapp/app/store/StoreAulasAsignadas.js
@@ -2,8 +2,8 @@ Ext.define('HOR.store.StoreAulasAsignadas',
 {
     extend : 'Ext.data.Store',
     autoLoad : false,
-    autoSync : true,    
-    model: 'HOR.model.AulaPlanificacion',
+    autoSync : true,
+    model : 'HOR.model.AulaPlanificacion',
     proxy :
     {
         type : 'rest',
@@ -19,6 +19,18 @@ Ext.define('HOR.store.StoreAulasAsignadas',
         writer :
         {
             type : 'json'
+        },
+        listeners :
+        {
+            exception : function(proxy, response, operation)
+            {
+                var msgList = response.responseXML.getElementsByTagName("msg");
+
+                if (msgList && msgList[0] && msgList[0].firstChild)
+                {
+                    Ext.Msg.alert("Error", msgList[0].firstChild.nodeValue);
+                }
+            }
         }
     }
 });
