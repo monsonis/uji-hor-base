97f674866dbb88e811f6a3f5360008b8c13c502d
Maria Sirvent <msirvent@uji.es>
2012-12-10 08:43:58 +0000

    issue HOR-197: Crear el objeto db para la vista de asignaturas comunes. http://jira.uji.es/browse/HOR-197
    Solucionado
    
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@12094 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/main/java/es/uji/apps/hor/db/AsignaturaComunDTO.java
@@ -0,0 +1,90 @@
+package es.uji.apps.hor.db;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * The persistent class for the HOR_EXT_ASIGNATURAS_COMUNES database table.
+ * 
+ */
+@SuppressWarnings("serial")
+@Entity
+@Table(name = "HOR_EXT_ASIGNATURAS_COMUNES")
+public class AsignaturaComunDTO implements Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "ASIGNATURA_ID")
+    private String asignaturaId;
+
+    @Column(name = "CURSO_ACADEMICO_ID")
+    private Long cursoAcademicoId;
+
+    @Column(name = "GRUPO_COMUN_ID")
+    private Long grupoComunId;
+
+    private String nombre;
+
+    public AsignaturaComunDTO()
+    {
+    }
+
+    public Long getId()
+    {
+        return this.id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getAsignaturaId()
+    {
+        return this.asignaturaId;
+    }
+
+    public void setAsignaturaId(String asignaturaId)
+    {
+        this.asignaturaId = asignaturaId;
+    }
+
+    public Long getCursoAcademicoId()
+    {
+        return this.cursoAcademicoId;
+    }
+
+    public void setCursoAcademicoId(Long cursoAcademicoId)
+    {
+        this.cursoAcademicoId = cursoAcademicoId;
+    }
+
+    public Long getGrupoComunId()
+    {
+        return this.grupoComunId;
+    }
+
+    public void setGrupoComunId(Long grupoComunId)
+    {
+        this.grupoComunId = grupoComunId;
+    }
+
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+
+}
