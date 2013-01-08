2aa052712e943c1eb6791b7c4d612185c75e8ba1
Javier Muñoz <ferrar@uji.es>
2013-01-08 09:21:23 +0000

    Eliminado DiaSemanaDAO y su Impl porque no se utilizaban en ninguna parte
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@12430 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/es/uji/apps/hor/dao/DiaSemanaDAO.java
+++
@@ -1,9 +0,0 @@
-package es.uji.apps.hor.dao;
-
-import es.uji.apps.hor.db.DiaSemanaDTO;
-import es.uji.commons.db.BaseDAO;
-
-public interface DiaSemanaDAO extends BaseDAO
-{
-    DiaSemanaDTO getDiaSemanaByNombre(String nombre);
-}
