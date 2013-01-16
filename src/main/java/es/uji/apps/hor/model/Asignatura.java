66283cfaf2690c72dd8d61fbe351d2a3a7de627c
Javier Muñoz <ferrar@uji.es>
2013-01-16 13:04:17 +0000

    Implementado método hashCode en Asignatura porque se implementó también el equals
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@12583 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/java/es/uji/apps/hor/model/Asignatura.java
+++ src/main/java/es/uji/apps/hor/model/Asignatura.java
@@ -134,6 +134,12 @@ public class Asignatura
     }
 
     @Override
+    public int hashCode()
+    {
+        return this.id.hashCode();
+    }
+
+    @Override
     public boolean equals(Object obj)
     {
         if (!(obj instanceof Asignatura))
