82d5f5b4a1d6f25e4a99b1e551029d12eaefee22
David Rubert <david.rubert@si.uji.es>
2013-01-17 12:18:28 +0000

    Solucionado un problema en un test de Siesta
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@12599 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/webapp/tests/010-DatesCursAcademic/020_semestredetallemodel.t.js
+++ src/main/webapp/tests/010-DatesCursAcademic/020_semestredetallemodel.t.js
@@ -7,7 +7,7 @@ StartTest(function(t) {
 	            nombreSemestre : 'Primer Semestre' 
 	        });
 	        
-	        t.is(mod.get('fecha_fin'), new Date(2012,8,24,0,0,0,0), 'Puedo leer la fecha de fin');
+	        t.is(mod.get('fechaFin'), new Date(2012,8,24,0,0,0,0), 'Puedo leer la fecha de fin');
 	        t.is(mod.get('nombreSemestre'), 'Primer Semestre', 'Puedo leer el nombre del semestre');
 	    });
 
