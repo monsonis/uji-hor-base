c8413973364ce3d721691363b78ea5bfebc40f13
David Rubert <david.rubert@si.uji.es>
2013-02-11 10:24:26 +0000

    Solucionado un problema con la desplanificación de un aula que tiene ya clases asignadas
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@13121 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/test/java/es/uji/apps/hor/dao/AulasDAOTest.java
+++ src/test/java/es/uji/apps/hor/dao/AulasDAOTest.java
@@ -9,7 +9,10 @@ import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import es.uji.apps.hor.AulaYaAsignadaAEstudioException;
@@ -43,7 +46,9 @@ import es.uji.apps.hor.model.TipoEstudio;
 import es.uji.apps.hor.model.TipoSubgrupo;
 import es.uji.commons.rest.exceptions.RegistroConHijosException;
 import es.uji.commons.rest.exceptions.RegistroNoEncontradoException;
-cations = { "classpath:applicationContext-test.xml" })
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext-test.xml" })
 public class AulasDAOTest
 {
     @Autowired
