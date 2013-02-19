75e0eb0481de33a4ef06db17d6d1bdb27101f029
David Rubert <david.rubert@si.uji.es>
2013-02-19 11:04:16 +0000

    IN PROGRESS - issue HOR-336: en la asignación de las aulas a clases, mostrar las plazas de las aulas http://jira.uji.es/browse/HOR-336
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@13343 e6edf6fb-f266-4316-afb4-e53d95876a76

--- src/main/webapp/app/model/AulaPlanificacion.js
+++ src/main/webapp/app/model/AulaPlanificacion.js
@@ -2,5 +2,12 @@ Ext.define('HOR.model.AulaPlanificacion',
 {
     extend : 'Ext.data.Model',
 
-    fields : [ 'id', 'nombre', 'estudioId', 'semestreId', 'aulaId', 'edificio', 'tipo', 'planta', 'plazas' ]
+    fields : [ 'id', 'nombre', 'estudioId', 'semestreId', 'aulaId', 'edificio', 'tipo', 'planta', 'plazas',
+    {
+        name : 'nombreYPlazas',
+        mapping : 'nombre',
+        convert: function(v, record) {
+            return v + " (" + record.data.plazas + " plaçes)"
+        }
+    } ]
 });
