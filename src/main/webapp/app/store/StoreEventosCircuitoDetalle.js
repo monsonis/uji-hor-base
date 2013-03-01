288eb619957806e65a85253c0f77b716cfa32971
Maria Sirvent <msirvent@uji.es>
2013-03-01 13:13:46 +0000

    issue HOR-367: Crear los Stores de gestión de circuitos http://jira.uji.es/browse/HOR-367
    Solucionado
    
    git-svn-id: http://jira.uji.es/repos/uji/HOR/uji-hor-base/trunk@13525 e6edf6fb-f266-4316-afb4-e53d95876a76

---
+++ src/main/webapp/app/store/StoreEventosCircuitoDetalle.js
@@ -0,0 +1,46 @@
+Ext.define('HOR.store.StoreEventosCircuitoDetalle',
+{
+    extend : 'Extensible.calendar.data.EventStore',
+    autoLoad : false,
+    proxy :
+    {
+        type : 'rest',
+        url : '/hor/rest/calendario/eventos/detalle/circuito',
+        noCache : false,
+
+        reader :
+        {
+            type : 'json',
+            root : 'data'
+        },
+
+        extraParams :
+        {
+            estudioId : null,
+            semestreId : null,
+            grupoId : null,
+            calendariosIds : null
+        },
+
+        writer :
+        {
+            type : 'json',
+            nameProperty : 'mapping'
+        },
+
+        listeners :
+        {
+            exception : function(proxy, response, operation, options)
+            {
+                var msg = response.message ? response.message : Ext.decode(response.responseText).message;
+                Ext.Msg.alert('Server Error', msg);
+            }
+        }
+    },
+
+    initComponent : function(cfg)
+    {
+        this.initConfig(cfg);
+        this.callParent(arguments);
+    }
+});
